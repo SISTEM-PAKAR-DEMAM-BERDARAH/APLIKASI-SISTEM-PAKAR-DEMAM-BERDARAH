41d7032d389598c0e891d5121920dcf9e4e65eba
Farul Wananda <farulahmad2002@gmail.com>
2023-03-20 11:47:02 +0700

    add: database migration v1.0

---
+++ app/Database/Migrations/2023-03-20-042136_Penyakit.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Penyakit extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'kode_penyakit' => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 3,
+                'unsigned'       => false,
+                'auto_increment' => false,
+            ],
+            'nama_penyakit' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'detail_penyakit' => [
+                'type' => 'TEXT',
+                'null' => false,
+            ],
+            'gambar' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+        ]);
+        $this->forge->addKey('kode_penyakit', true);
+        $this->forge->createTable('penyakit');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('penyakit');
+    }
+}
